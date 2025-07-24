9254644fc115c6a6709086f7074fd5dfe2471edd
ZaidAsif <zaid.asif.9082@gmail.com>
2025-07-25 03:58:06 +0500

    solving strict type errors

--- src/types/appointment.ts
+++ src/types/appointment.ts
@@ -1,16 +1,6 @@
+import { Application } from "./application";
 import { AppointmentDetails } from "./appointmentDetail";
-import { User } from "./user";
 
-export type Appointment = {
-  _id: string;
-  userId: User;
-  loanId: string;
-  date: string; 
-  time: string; 
-  token: string; 
-  location: string;
-  slipUrl: string; 
-  createdAt: Date;
-  category: string;
-  appointmentDetails: AppointmentDetails
+export interface Appointment extends Application {
+  appointmentDetails: AppointmentDetails;
 }
